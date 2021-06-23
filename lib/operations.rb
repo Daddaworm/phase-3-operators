56b3d1ca96ac2c2348fcfa3415d387737521e5e4
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-23 16:14:32 -0400

    Completed assignment

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+    if speed > 60
+        return true
+    end
+    if speed < 40
+        return true
+    else
+        return false
+    end
 end
 
 
+# unsafe?()
 
 def not_safe?(speed)
-	
+    speed < 40 || speed > 60 ? true : false
 end
 	
+
 
 
